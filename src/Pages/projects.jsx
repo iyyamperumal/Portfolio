bccc660ae95e1630759b2edc4237b4ca2f3bc7f6
iyyamperumal <sathishkannan140@gmail.com>
2025-02-21 14:06:10 +0530

    Project section updated with card types

--- src/Pages/projects.jsx
+++ src/Pages/projects.jsx
@@ -1,50 +1,49 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import travel from '../assets/images/travelplanner.jpg';
 import landing from '../assets/images/landingpage.jpg';
 import todo from '../assets/images/todo.jpg';
+import ajstock from '../assets/images/ajstocks.jpg';
 const Projects = () => {
     return (
         <>
             <div className='projects' id='projects'>
                 <p className='text-sky-300 roboto-condensed-head underline h1 pl-2 pt-10'>Projects</p>
-                <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
-                    <ol className="carousel-indicators">
-                        <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
-                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                    </ol>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active justify-items-center">
-                            <img src={travel} className="d-block w-4/5 blur-sm" alt="..." />
-                            <div className="carousel-caption d-md-block pb-1 sm:pb-0">
-                                <p className='text-black lg:text-4xl md:text-3xl text-base bungee-shade-regular'>Travel Planner Application</p>
-                                <span className='text-black lg:text-2xl md:text-xl text-base underline flex flex-row justify-evenly'><a href='https://travelwithourapp.netlify.app' target='blank'>View site</a> <a href='https://github.com/iyyamperumal/Travel-FE' target='blank'>Github</a></span>
-                            </div>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{
+                        duration: 1.3,
+                        ease: "easeOut",
+                    }}
+                    className="text-[0.8rem] overflow-x-auto md:overflow-x-visible lg:text-lg text-justify content-center lg:text-right lg:w-full lg:h-full lg:pt-10 pt-25 lg:p-0 p-3 w-[100%]"
+                ><div className="flex flex-row gap-5 josefin-sans-about justify-center text-opacity-100 lg:w-full w-[200%] lg:h-full md:w-full h-[200%]">
+                        <div className="shadow-lg shadow-gray-500 flex flex-col justify-between cursor-pointer hover:shadow-xl border border-white md:text-sm w-[30%] h-[10rem] md:w-[10rem] md:h-[10rem] lg:w-[15rem] lg:h-[15rem] bg-opacity-10 rounded-md p-1 hover:p-3 transition-all duration-200 will-change-transform hover:scale-105"
+                        >
+                            <img src={travel} className="d-block w-full h-1/2 rounded-md" alt="..." />
+                            <p className="text-white text-xl text-opacity-100 text-center">Travel planner Application</p>
+                            <span className='text-black lg:text-lg md:text-lg text-base underline flex flex-row justify-evenly'><a href='https://travelwithourapp.netlify.app' target='blank'>View site</a> <a href='https://github.com/iyyamperumal/Travel-FE' target='blank'>Github</a></span>
                         </div>
-                        <div className="carousel-item justify-items-center">
-                            <img src={landing} className="d-block w-4/5 blur-sm" alt="..." />
-                            <div className="carousel-caption d-md-block">
-                                <p className='text-black lg:text-4xl md:text-3xl text-base rubik-glitch-regular'>Landing Page</p>
-                                <span className='text-black lg:text-2xl md:text-xl text-base underline flex flex-row justify-evenly'><a href='https://componentstask.netlify.app' target='blank'>View site</a> <a href='https://github.com/iyyamperumal/Task13' target='blank'>Github</a></span>
-                            </div>
+                        <div className="shadow-lg shadow-gray-500 flex flex-col justify-between cursor-pointer hover:shadow-xl border border-white md:text-sm w-[30%] h-[10rem] md:w-[10rem] md:h-[10rem] lg:w-[15rem] lg:h-[15rem] bg-opacity-10 rounded-md p-1 hover:p-3 transition-all duration-200 will-change-transform hover:scale-105"
+                        >
+                            <img src={todo} className="d-block w-full h-1/2 rounded-md" alt="..." />
+                            <p className="text-white text-xl text-opacity-100 text-center">ToDo List Application</p>
+                            <span className='text-black lg:text-lg md:text-lg text-base underline flex flex-row justify-evenly'><a href='https://lsttodo.netlify.app' target='blank'>View site</a> <a href='https://github.com/iyyamperumal/Task14' target='blank'>Github</a></span>
                         </div>
-                        <div className="carousel-item justify-items-center">
-                            <img src={todo} className="d-block w-4/5 blur-sm" alt="..." />
-                            <div className="carousel-caption d-md-block">
-                                <p className='text-black lg:text-4xl md:text-3xl text-base rubik-vinyl-regular'>To Do Application</p>
-                                <span className='text-black lg:text-2xl md:text-xl text-base underline flex flex-row justify-evenly'><a href='https://lsttodo.netlify.app' target='blank'>View site</a> <a href='https://github.com/iyyamperumal/Task14' target='blank'>Github</a></span>
-                            </div>
+                        <div className="shadow-lg shadow-gray-500 flex flex-col justify-between cursor-pointer hover:shadow-xl border border-white md:text-sm w-[30%] h-[10rem] md:w-[10rem] md:h-[10rem] lg:w-[15rem] lg:h-[15rem] bg-opacity-10 rounded-md p-1 hover:p-3 transition-all duration-200 will-change-transform hover:scale-105"
+                        >
+                            <img src={landing} className="d-block w-full h-1/2 rounded-md" alt="..." />
+                            <p className="text-white text-xl text-opacity-100 text-center">Landing Page</p>
+                            <span className='text-black lg:text-lg md:text-lg text-base underline flex flex-row justify-evenly'><a href='https://componentstask.netlify.app' target='blank'>View site</a> <a href='https://github.com/iyyamperumal/Task13' target='blank'>Github</a></span>
+                        </div>
+                        <div className="shadow-lg shadow-gray-500 flex flex-col justify-between cursor-pointer hover:shadow-xl border border-white md:text-sm w-[30%] h-[10rem] md:w-[10rem] md:h-[10rem] lg:w-[15rem] lg:h-[15rem] bg-opacity-10 rounded-md p-1 hover:p-3 transition-all duration-200 will-change-transform hover:scale-105"
+                        >
+                            <img src={ajstock} className="d-block w-full h-1/2 rounded-md" alt="..." />
+                            <p className="text-white text-xl text-opacity-100 text-center">Aj Stocks</p>
+                            <span className='text-black lg:text-lg md:text-lg text-base underline flex flex-row justify-evenly'><a href='https://ajstock.netlify.app/' target='blank'>View site</a></span>
                         </div>
                     </div>
-                    <button className="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Next</span>
-                    </button>
-                </div>
+                </motion.div>
             </div>
         </>
     );
